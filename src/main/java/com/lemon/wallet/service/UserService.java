3e7af567d5906f2a357fd2aaa2887fa0c2185d8b
Lucas Gianotti <gianotti.92@gmail.com>
2021-05-14 18:06:10 -0300

    Se borra metodo sin uso

--- src/main/java/com/lemon/wallet/service/UserService.java
+++ src/main/java/com/lemon/wallet/service/UserService.java
@@ -1,7 +1,6 @@
 package com.lemon.wallet.service;
 
 import com.lemon.wallet.client.PersistenceUserClient;
-import com.lemon.wallet.exception.ApiException;
 import com.lemon.wallet.model.User;
 import org.springframework.stereotype.Service;
 
@@ -20,15 +19,5 @@ public class UserService {
         Optional<User> optionalUserPersisted = persistenceUserClient.find(user.getEmail(), user.getAlias());
 
         return optionalUserPersisted.orElseGet(() -> persistenceUserClient.save(user));
-    }
-
-    public User find(Long id) {
-       Optional<User> optionalUser = persistenceUserClient.find(id);
-
-       if(optionalUser.isPresent()) {
-           return optionalUser.get();
-       }
-
-       return optionalUser.orElseThrow(() -> new ApiException(String.format("User not fund, with id: %s", id)));
     }
 }
